25826989b54123e6f2a00cba251d7f91a6b1d90b
Fernando Andrés <fandresfernanduelo@gmail.com>
2022-02-22 15:28:21 +0100

    Creado compoente ListOfGifs

--- src/App.js
+++ src/App.js
@@ -1,29 +1,22 @@
 import React, { useEffect, useState} from 'react'; 
 import './App.css';
 import getGifs from './services/getGifs';
-import Gif from './components/Gif';
+import ListOfGifs from './components/ListOfGifs';
 
 
 function App() {
   const [gifs, setGifs] =  useState([]);
+
   useEffect(() => {
-    getGifs('porsche', 4).then(gifs => setGifs(gifs));
+    getGifs('git', 4).then(gifs => setGifs(gifs));
   }, []); //El segundo parámetro del useEffect indica las dependencias, el parametro que debe cambiar para volver a renderizar el componente
           //En este caso es un array vacío, eso indica que solo se ejecuta la primera vez que se renderiza
   return (
     <div className="App">
       <section className='App-content'>
       <ol>
-        {
-          gifs.map(singleGif => 
-          <Gif
-            key={singleGif.id}
-            title={singleGif.title} 
-            url={singleGif.url} 
-            id={singleGif.id}/>
-          )
-        }
-        </ol>
+        <ListOfGifs gifs = {gifs}/>
+      </ol>
       </section>
     </div>
     );
--- src/index.js
+++ src/index.js
@@ -4,35 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-/*const useState = React.useState;
-
-const $app = document.querySelector('#app');
-
-const Avatar = ({id, name = "Anonymus", size}) => {
-  const [enabled, setEnabled] = useState(true);
-  const imgClassName = enabled ? '' : 'disabled';
-  const pictureClassName = size === 'small' ? 'is-small' : '';
-  const src = `https://randomuser.me/api/portraits/women/${id}.jpg`;
-  return(
-  <picture className={pictureClassName}>
-    {id ? (
-      <img alt="Icono de usuario" onClick = {() => setEnabled(!enabled)} className={imgClassName}  src={src}/>
-    ) : (<img alt='Icono de usuario' src='https://randomuser.me/api/portraits/lego/3.jpg'/>
-      )} 
-  <em>{name}</em>
-  </picture>
-  );
-};
-
-ReactDOM.render(
-  <div>
-    <Avatar id={11} name="Elisabeth"/>
-    <Avatar id={12} name="Putilla"/>
-    <Avatar id={22}/>
-    <Avatar/>
-  </div>
-  , $app);
-*/
 ReactDOM.render(
     <App />,
   document.getElementById('root')
