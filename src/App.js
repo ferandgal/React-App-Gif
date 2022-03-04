b9a27a99ef3958b02c7ac1969f4d3ef74c64f6be
Fernando Andrés <fandresfernanduelo@gmail.com>
2022-03-04 20:55:15 +0100

    Barra de búsqueda de gif

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import './App.css';
 import ListOfGifs from './components/ListOfGifs';
 
-import { Link, Route} from "wouter"
+import { Link, Route, useLocation} from "wouter"
+import React, { useState } from 'react';
 
 export default function App() {
+  const [keyword, setKeyword] = useState('');
+  const [path, pushLocation] = useLocation();
+
+  const handleSubmit = evt => {
+    evt.preventDefault();
+    console.log(keyword);
+    pushLocation(`/gif/${keyword}/5`);
+  }
+  //Cada vez que se cambia el input se cambia el estado
+  const handleChange = evt => {
+    setKeyword(evt.target.value);
+  }
+  
   return (
     <div className="App">
       <section className='App-content'>
         <h1>Buscador de Gifs</h1>
-        <Link to='/gif/git/3'>El nano campeón</Link>
+        <form onSubmit={handleSubmit}>
+          <input onChange={handleChange} placeholder="Busca un gif" type='text' value={keyword} />
+          <input type="submit" value="Buscar"/>
+        </form>
+        <Link to='/gif/git/3'>Git</Link>
         <Route 
           component = {ListOfGifs}
           path="/gif/:keyword/:limit" />
